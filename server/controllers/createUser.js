90d98b091d150764b9d76f6e8edc88797666cb7f
Pacifique kibaka <irapacifique1@gmail.com>
2019-09-04 21:29:24 +0200

    bug(api): refactoring create user api [finishes #168300932]

--- server/controllers/createUser.js
+++ server/controllers/createUser.js
@@ -1,20 +1,21 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import user_data from '../models/user_data';
 import giveResponse from '../helpers/response';
 import {validate} from "../middleware/validateUser";
 
-
+const { responses } = giveResponse;
 const createUser = async (req, res) => {
                 //joi
 const { error } = validate(req.body); 
-if (error) return res.status(400).send(error.details[0].message);
+if (error) return res.status(401).send(error.details[0].message);
               //joi end    
   const {first_name,last_name,email,password,address,bio,occupation,expertise,is_admin} = req.body;
 
   let checkUser = user_data.find(user => user.email === req.body.email);
-  if (checkUser) return res.status(400).send('User already registered.');
+  if (checkUser) return responses(res,409,null, true, 'User already exist' );
   
-  const newUser = {
+  let newUser = {
     id: user_data.length + 1,
     first_name,
     last_name,
@@ -27,10 +28,17 @@ if (error) return res.status(400).send(error.details[0].message);
     is_admin
   };
 
- const salt = await bcrypt.genSalt(10);
+ const salt = await bcrypt.genSalt(10); // await is an operator used to wait for promise to resolve or reject
  newUser.password = await bcrypt.hash(newUser.password, salt);
  user_data.push(newUser);
- giveResponse.responses(res, 201, newUser);
+ 
+ //generate token
+ const token=jwt.sign({id:newUser.id},process.env.JWT_SECRET,{expiresIn:"24h"});
+ // omit password
+ // eslint-disable-next-line no-unused-vars
+ const { password : _, ...omitpUser } = newUser;
+ 
+    return responses(res, 201, { token,...omitpUser }, false, 'User created successfully');
   
     }
 
