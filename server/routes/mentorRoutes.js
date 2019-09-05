b06c75bbca85c3c618c34b5989fcbd1d1c24ca5c
Pacifique kibaka <irapacifique1@gmail.com>
2019-09-05 11:23:14 +0200

    bug(API): refactoring get all mentors endpoint [finishes #168309300]

--- server/routes/mentorRoutes.js
+++ server/routes/mentorRoutes.js
@@ -1,28 +1,12 @@
+import auth from '../middleware/checkAuth';
 import express from 'express';
 import mentorController from '../controllers/controllerMentor';
-//import checkToken from '../middleware/userToken';
 
 
 const router = express.Router();
 
 /*route to get all mentors*/
-router.get('/mentors', mentorController.getAllMentors);
-
-/*route to get a specific mentor*/
-router.get('/mentors/:id', mentorController.getSingleMentor);
+router.get('/mentors',[auth,mentorController.getAllMentors] );
 
 export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
